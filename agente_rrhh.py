b7608897d1755c481bdd7c2ad7f74f2ef06d529b
Jesus Mago <jesusalejandromagomalave@gmail.com>
2026-04-24 17:08:17 -0400

    actualizacion del models

--- agente_rrhh.py
+++ agente_rrhh.py
@@ -76,7 +76,7 @@ if st.button("🚀 Analizar Candidatos"):
                 try:
                     # Llamada a la nueva SDK
                     response = client.models.generate_content(
-                        model='gemini-1.5-flash',
+                        model='models/gemini-1.5-flash',
                         contents=prompt,
                         config={
                             'response_mime_type': 'application/json',
